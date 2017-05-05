a539355937085874d695b1b3e64c829d2eabe61e
lionelmoses <lionelmoses@gmail.com>
2017-05-05 18:16:43 +0530

    Add files via upload

---
+++ Session 5/Assignment 5.3.js
@@ -0,0 +1,14 @@
+//Objects and nested objects
+
+var Employee = {
+				Name : 'Lionel', 
+				Age : 29,
+				Salary : 1000,
+				Address : 'Electronic City',				
+				DetailAddress: {
+					City : 'Bangalore',
+					State : 'Karnataka',
+					Pincode : 560100}
+				};
+
+console.log(Employee);
---
+++ Session 5/Assignment 5.4.js
@@ -0,0 +1,8 @@
+//Arrays
+
+var Employee = [];
+Employee[0] = ['Lionel', 29, 65000, 'Bangalore'];
+Employee[1] = ['Moses', 27, 55000, 'Chennai'];
+Employee[2] = ['Lio', 26, 85000, 'Kochi'];
+
+console.log(Employee);
